94a53051b5416cf7f3705c1056eeae6bbf2462bd
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-11 19:45:07 +0100

    pad_setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-import 'App.css'
+import './App.css'
 
 export default class App extends Component {
+  state = {
+    keys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+  }
+
   render() {
+    const { keys } = this.state;
+
     return (
-      <div>App</div>
+      <div className='App'>
+        <div className='pad'>
+          {keys.map((key, index) => (
+              <Button text={key} key={index}/>
+          ))}
+        </div>
+      </div>
     )
   }
 }
+
+const Button = (props) => (
+  <div className='button'>
+    {props.text}
+  </div>
+);
 
