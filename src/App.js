f51a25a1ed3179b74e4e1f707dcf860049f7d850
ondrejvosmera <ondravosmera@seznam.cz>
2022-11-15 20:34:45 +0100

    linked_sounds

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react'
 import './App.css'
 
-export default class App extends Component {
-  state = {
-    keys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+const sounds = [
+  {
+    key: 'Q',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    key: 'W',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    key: 'E',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    key: 'A',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    key: 'S',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    key: 'D',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    key: 'Z',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    key: 'X',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  },
+  {
+    key: 'C',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   }
+]
+ 
+const App = () => (
+  <div className='App'>
+    <div className='pad'>
+      {sounds.map((sound, index) => (
+          <Button text={sound.key} key={index} audio={sound.mp3}/>
+       ))}
+     </div>
+  </div>
+);
 
+class Button extends React.Component {
   render() {
-    const { keys } = this.state;
-
     return (
-      <div className='App'>
-        <div className='pad'>
-          {keys.map((key, index) => (
-              <Button text={key} key={index}/>
-          ))}
-        </div>
+      <div className='button'>
+        {this.props.text}
+        <audio />
       </div>
     )
   }
 }
 
-const Button = (props) => (
-  <div className='button'>
-    {props.text}
-  </div>
-);
-
+export default App;
